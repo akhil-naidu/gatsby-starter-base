a504fae23e8f940c39d8f272a6983fb752f78fa4
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-06-27 17:31:58 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,19 +6,5 @@ module.exports = {
     description: "A community for Cyber Security experts and a Starting node for beginners ",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: [
-    `@reflexjs/gatsby-theme-base`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Leeward`,
-        short_name: `Leeward`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#000`,
-        display: `standalone`,
-        icon: `content/images/favicon.png`,
-      },
-    },
-  ],
+  plugins: ["@reflexjs/gatsby-theme-base"],
 }
