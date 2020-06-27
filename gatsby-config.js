a8007a77eccfcbb0038fd75b57fc0f1f17814ecf
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-06-27 13:50:42 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: "Leewardslope",
+    title: "Leeward",
     description: "A community for Cyber Security experts and a Starting node for beginners ",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
