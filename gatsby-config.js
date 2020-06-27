92f794dee3e544ee31e158af1c6489379716d1bf
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-06-27 13:38:35 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require(`dotenv`).config()
 module.exports = {
   siteMetadata: {
     title: "Leewardslope",
-    description: "Starter for the Reflex.",
+    description: "A community for Cyber Security experts and a Starting node for beginners ",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: ["@reflexjs/gatsby-theme-base"],
