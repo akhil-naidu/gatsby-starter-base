e13e18aa7fda3d9c0f05c643eb67e2200dd0ea30
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-06-27 17:21:16 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,23 @@ module.exports = {
     description: "A community for Cyber Security experts and a Starting node for beginners ",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: ["@reflexjs/gatsby-theme-base"],
+  plugins: [
+    `@reflexjs/gatsby-theme-base`,
+    `@reflexjs/gatsby-theme-doc`,
+    `@reflexjs/gatsby-theme-library`,
+    `@reflexjs/gatsby-theme-tutorial`,
+    `@reflexjs/gatsby-theme-styleguide`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Reflex`,
+        short_name: `Reflex`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#000`,
+        display: `standalone`,
+        icon: `content/images/favicon.png`,
+      },
+    },
+  ],
 }
